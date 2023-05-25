595ba713ac2c1a734c9b811ac77c971d4f7a1397
Codekacode <karem.quiroz.m@gmail.com>
2023-05-25 16:46:07 -0500

    Add Info component

---
+++ src/Info/Info.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Info.css';
+
+const Info = () => {
+  return (
+    <div>
+      <div className="info-left">
+        <div className="contact"></div>
+        <nav className="links"></nav>
+      </div>
+      <div className="info-right">
+        <div className="social"></div>
+        <div className="credits"></div>
+        <div className="color-mode"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Info
